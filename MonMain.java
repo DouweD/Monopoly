26d87d27594ff8f514603d8e23c019d1cd4b2e33
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-16 16:26:58 +0100

    stap 1 - steden en straten met lijst

--- MonMain.java
+++ MonMain.java
@@ -2,7 +2,19 @@
 public class MonMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Stad amsterdam = new Stad();
+		amsterdam.naamStad = "Amsterdam";
+		amsterdam.straten.add("Dorpsstraat");
+		System.out.println(amsterdam.straten);
+		
+		Stad rotterdam = new Stad();
+		rotterdam.naamStad = "Rotterdam";
+		System.out.println(rotterdam.naamStad);
+		
+		Stad utrecht = new Stad();
+		utrecht.naamStad = "Utrecht";
+		System.out.println(utrecht.naamStad);
 
 	}
 
--- Stad.java
+++ Stad.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Stad {
 	
-	
-	ArrayList<String> stratenAmsterdam = new ArrayList<>();
+	String naamStad;
+	ArrayList<String> straten = new ArrayList<>();
 	
 }
