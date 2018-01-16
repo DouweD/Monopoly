bd3b5c992fe855042e42adc86233f963d7dd62ab
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-16 16:34:39 +0100

    stap 1 update

--- MonMain.java
+++ MonMain.java
@@ -6,15 +6,18 @@ public class MonMain {
 		Stad amsterdam = new Stad();
 		amsterdam.naamStad = "Amsterdam";
 		amsterdam.straten.add("Dorpsstraat");
-		System.out.println(amsterdam.straten);
+		System.out.println(amsterdam.naamStad + " heeft ook een " + amsterdam.straten);
 		
 		Stad rotterdam = new Stad();
 		rotterdam.naamStad = "Rotterdam";
-		System.out.println(rotterdam.naamStad);
+		rotterdam.straten.add("Dorpsstraat");
+		rotterdam.straten.add("Stationsstraat"); 
+		System.out.println(rotterdam.naamStad + " heeft de volgende straten: " + rotterdam.straten);
 		
 		Stad utrecht = new Stad();
 		utrecht.naamStad = "Utrecht";
-		System.out.println(utrecht.naamStad);
+		utrecht.straten.add("Dorpsstraat");
+		System.out.println(utrecht.naamStad+ " heeft ook een " + utrecht.straten);
 
 	}
 
