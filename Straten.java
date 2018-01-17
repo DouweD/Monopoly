1779b9517baa4cd09d25238f237b23b45bbfcfeb
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-17 23:32:10 +0100

    Straten erbij

---
+++ Straten.java
@@ -0,0 +1,19 @@
+
+public class Straten {
+	
+	private int prijs;
+	
+	public void setPrijs(int strPrijs) {
+		prijs = strPrijs;
+		
+	}
+	
+	public int getPrijs(int prijs) {
+		return prijs;
+	}
+	
+	String[] straatnamen = {"Dorpsstraat", "Stationsstraat", "Hoofdstraat"};
+	
+	
+	
+}
